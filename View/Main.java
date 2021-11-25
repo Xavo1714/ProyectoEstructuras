286239a3842bd9ef1d3482d16999e1e96c125c29
Robdres <robdres123@gmail.com>
2021-11-25 01:14:50 -0500

    View

--- View/Main.java
+++ View/Main.java
@@ -1,20 +1,19 @@
-
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
 	public void start(Stage primaryStage) throws Exception{
 		Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
 		Scene scene = new Scene(root);
-		primaryStage.setTitle("IndiceInvertido");
+		primaryStage.setTitle("QuickSort");
 		primaryStage.setScene(scene);
 		primaryStage.show();
-	}
 
+	}
 	public static void main(String[] args){
 		launch(args);
 	}
